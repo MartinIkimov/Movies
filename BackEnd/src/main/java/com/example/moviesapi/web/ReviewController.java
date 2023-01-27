37499e4b33fa2a1d49b25ae05f14383f82be228a
MartinIkimov <martin_ikimov2@abv.bg>
2023-01-27 19:33:38 +0200

    Done with the React part of the application

--- BackEnd/src/main/java/com/example/moviesapi/web/ReviewController.java
+++ BackEnd/src/main/java/com/example/moviesapi/web/ReviewController.java
@@ -4,15 +4,13 @@ import com.example.moviesapi.model.Review;
 import com.example.moviesapi.service.ReviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ReviewController {
 
     private ReviewService reviewService;
